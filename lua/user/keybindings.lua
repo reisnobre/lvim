e94412dc38bb9ec2b27c5e5ff8c13b7c6e90bb09
Eddy R. Nobre <eduardo.r@dotcubed.io>
2021-08-30 19:22:00 -0300

    Better mappings for which_key related to git

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -9,23 +9,32 @@ M.config = function ()
   lvim.builtin.which_key.mappings.e = nil -- Removed old NvimTreeToggle
   lvim.builtin.which_key.mappings.n = { ":NvimTreeToggle<CR>", "Toggle NvimTree" }
 
+  lvim.builtin.which_key.mappings["g"] = {
+    name = "Git",
+    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+    a = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    u = { "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk" },
+    s = { "<cmd>G<cr>", "Fugitive Git" }
+  }
+
   lvim.builtin.which_key.mappings["f"] = {
     name = "+Telescope",
     p = { "<cmd>Telescope find_files<cr>", "Find files" },
     a = { "<cmd>Telescope live_grep<cr>", "Grep files" },
     b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+    c = { "<cmd>Telescope colorscheme<cr>", "ColorSchemes" },
     g = {
       name = "+Git",
       c = { "<cmd>Telescope git_commits<cr>", "Git commits" },
+      C = { "<cmd>Telescope git_bcommits<cr>", "Checkout commit(for current file)" },
       b = { "<cmd>Telescope git_branches<cr>", "Git branches" },
       s = { "<cmd>Telescope git_status<cr>", "Git status" }
     }
-  }
-
-  lvim.builtin.which_key.mappings["G"] = {
-    name = "+Fugitive",
-    s = { "<cmd>G<cr>", "Git" },
-    c = { "<cmd>G<cr>", "Git commit" }
   }
 end
 
