742e011299b60441baede487fc9b8e40f7dffa95
Eddy R. Nobre <eduardo.reisnobre@gmail.com>
2025-02-28 15:15:50 -0300

    chore: converted keybindings to new style provided by which-key

--- lua/user/config/keybindings.lua
+++ lua/user/config/keybindings.lua
@@ -1,124 +1,89 @@
 local M = {}
 
+local prefix = "<leader>"
+
 M.config = function ()
   lvim.keys.insert_mode["jj"] = "<Esc>"
 
   -- Buffer Navigation
-  lvim.keys.normal_mode["<leader><leader>"] = "<cmd>Telescope find_files<cr>"
+  lvim.keys.normal_mode["<leader><leader>"] = ":Telescope find_files<cr>"
   lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
   lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-  lvim.keys.normal_mode["gr"] = "<cmd>Trouble lsp_references<cr>"
+  lvim.keys.normal_mode["gr"] = ":Trouble lsp_references<cr>"
 
   -- Additional Leader bindings for WhichKey
-  lvim.builtin.which_key.mappings.s = { ":update<cr>", "QuickSave" }
-  lvim.builtin.which_key.mappings.n = { ":NvimTreeToggle<CR>", "Toggle NvimTree" }
-  lvim.builtin.which_key.mappings.T.p = { "<cmd>TSPlaygroundToggle<cr>", "Playground" }
+  lvim.builtin.which_key.user = {
+    { prefix .. "n", ":NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+    { prefix .. "s", ":update<CR>", desc = "QuickSave" },
+    -- Git
+    { prefix .. "g", group = "Git" },
+    { prefix .. "gs", ":G<CR>", desc = "Fugitive" },
+    { prefix .. "ga", ":lua require 'gitsigns'.stage_hunk()<cr>", desc = "State hunk" },
+    -- Bufferline
+    { prefix .. "b", group = "BufferLine" },
+    { prefix .. "bh", cmd = ":BufferLineCloseLeft<cr>", desc = "close all buffers to the left" },
+    { prefix .. "bl", cmd = ":BufferLineCloseRight<cr>", desc = "close all buffers to the right" },
+    { prefix .. "bj", cmd = ":BufferLinePick<cr>", desc = "jump to a buffer" },
+    { prefix .. "bJ", cmd = ":BufferLinePickClose<cr>", desc = "close target buffer" },
+    { prefix .. "bt", cmd = ":BufferLineGroupToggle front<CR>", desc = "toggle front tabs" },
+    { prefix .. "bT", cmd = ":BufferLineGroupToggle back<CR>", desc = "toggle back tabs" },
+    { prefix .. "bD", cmd = ":call delete(expand('%')) | bdelete!<CR>", desc = "Delete current file" },
+    { prefix .. "by", cmd = ":let @+ = expand('%')<CR>", desc = "Copy path to current file" },
+    -- Telescope
+    { prefix .. "f", group = "Telescope" },
+    { prefix .. "fp", cmd = ":Telescope find_files<cr>", desc = "Find files" },
+    { prefix .. "fa", cmd = ":Telescope live_grep<cr>", desc = "Grep files" },
+    { prefix .. "fo", cmd = ":Telescope oldfiles<cr>", desc = "Old files" },
+    { prefix .. "fm", cmd = ":Telescope marks<cr>", desc = "Marks" },
+    { prefix .. "fb", cmd = ":lua require('user.telescope.custom-finders').curbuf()<cr>", desc = "Current Buffer" },
+    { prefix .. "fs", cmd = ":Telescope projects<cr>", desc = "Projects" },
+    { prefix .. "fP", cmd = ":lua require('user.telescope.custom-finders').project_search()<cr>", desc = "Project" }, -- Broken
 
+    -- Git Group
+    { prefix .. "fgg", cmd = ":lua require('user.telescope.custom-finders').git_files()<cr>", desc = "Git Files" },
+    { prefix .. "fgc", cmd = ":Telescope git_commits<cr>", desc = "Git commits"},
+    { prefix .. "fgC", cmd = ":Telescope git_bcommits<cr>", desc = "Checkout commit (for current file)"},
+    { prefix .. "fgb", cmd = ":Telescope git_branches<cr>", desc = "Git branches"},
+    { prefix .. "fgs", cmd = ":lua require('user.telescope.custom-finders').git_status()<cr>", desc = "Git Status"},
 
-  -- Additional git command
-  lvim.builtin.which_key.mappings.g.s = { "<cmd>G<cr>", "Fugitive Git" }
-  lvim.builtin.which_key.mappings.g.a = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "State hunk" }
+    -- LSP Group
+    { prefix .. "fla", cmd = ":lua require('user.telescope.custom-finders').code_actions()<CR>", desc = "Code Actions"},
 
+    -- Trouble
+    { prefix .. "t", group = "Trouble" },
+    { prefix .. "r", cmd = ":Trouble lsp_references<cr>", desc = "References" },
+    { prefix .. "f", cmd = ":Trouble lsp_definitions<cr>", desc = "Definitions" },
+    { prefix .. "i", cmd = ":Trouble lsp_implementations<cr>", desc = "Implementations" },
+    { prefix .. "q", cmd = ":Trouble quickfix<cr>", desc = "QuickFix" },
+    { prefix .. "l", cmd = ":Trouble loclist<cr>", desc = "LocationList" },
+    { prefix .. "w", cmd = ":Trouble lsp_workspace_diagnostics<cr>", desc = "Diagnostics" },
+    -- Spectre
+    { prefix .. "R", group = "Spectre" },
+    { prefix .. "Rp", cmd = ":lua require('spectre').open()<cr>", desc = "Project" },
+    { prefix .. "Rf", cmd = ":lua require('spectre').open_file_search({select_word=true})<cr>", desc = "Current Buffer" },
+    { prefix .. "Rw", cmd = ":lua require('spectre').open_visual({select_word=true})<cr>", desc = "Replace Word" },
+    -- Neorg
+    { prefix .. "o", group = "Neorg" },
+    { prefix .. "on", cmd = ":Plug>(neorg.dirman.new-note)", desc = "New note" },
+    { prefix .. "oh", cmd = ":Plug>(neorg.esupports.hop.hop-link)", desc = "Jump to Link" },
+    { prefix .. "oH", cmd = ":Plug>(neorg.esupports.hop.hop-link.vsplit)", desc = "Open link vsplit" },
+    { prefix .. "ot", cmd = ":Neorg toggle-concealer<CR>", desc = "Toggle concealer" },
+    { prefix .. "or", cmd = ":Neorg return<CR>", desc = "Return" },
+    { prefix .. "oi", cmd = ":Neorg index<CR>", desc = "Index" },
+    { prefix .. "ow", cmd = ":Neorg workspace ", desc = "Workspaces" },
+    { prefix .. "om", cmd = ":Neorg inject-metadata<CR>", desc = "Inject metadata" },
+    { prefix .. "oM", cmd = ":Neorg update-metadata<CR>", desc = "Update metadata" },
+    { prefix .. "oS", cmd = ":Neorg generate-workspace-summary<CR>", desc = "Insert summary" },
+    { prefix .. "oj", group = "Journal" },
+    -- Journal Group
+    { prefix .. "ojt", cmd = ":Neorg journal today<CR>", desc = "Today", group = "Journal" },
+    { prefix .. "ojy", cmd = ":Neorg journal yesterday<CR>", desc = "Yesterday", group = "Journal" },
+    { prefix .. "ojc", cmd = ":Neorg journal custom<CR>", desc = "Custom", group = "Journal" },
 
-  lvim.builtin.which_key.mappings["b"] = {
-    name = "+bufferline",
-    h = { "<cmd>BufferLineCloseLeft<cr>", "close all buffers to the left" },
-    l = { "<cmd>BufferLineCloseRight<cr>", "close all BufferLines to the right" },
-    j = { ":BufferLinePick<cr>", "jump to a buffer" },
-    J = { ":BufferLinePickClose<cr>", "close target buffer" },
-    t = { ":BufferLineGroupToggle front<CR>", "toggle front tabs" },
-    T = { ":BufferLineGroupToggle back<CR>", "toggle back tabs" },
-    D = { ":call delete(expand('%')) | bdelete!<CR>", "Delete current file" },
-    y = { ":let @+ = expand('%')<CR>", "Copy path to current file" },
-  }
-
-  -- Telescope keybindings
-  lvim.builtin.which_key.mappings["f"] = {
-    name = "+Telescope",
-    p = { "<cmd>Telescope find_files<cr>", "Find files" },
-    a = { "<cmd>Telescope live_grep<cr>", "Grep files" },
-    o = { "<cmd>Telescope oldfiles<cr>", "Old files" },
-    m = { "<cmd>Telescope marks<cr>", "Marks" },
-    b = { "<cmd>lua require('user.telescope.custom-finders').curbuf()<cr>", "Current Buffer" },
-    s = { "<cmd>Telescope projects<cr>", "Projects" },
-    P = { "<cmd>lua require('user.telescope.custom-finders').project_search()<cr>", "Project" }, -- Broken
-    g = {
-      name = "+Git",
-      g = { "<cmd>lua require('user.telescope.custom-finders').git_files()<cr>", "Git Files" },
-      c = { "<cmd>Telescope git_commits<cr>", "Git commits" },
-      C = { "<cmd>Telescope git_bcommits<cr>", "Checkout commit(for current file)" },
-      b = { "<cmd>Telescope git_branches<cr>", "Git branches" },
-      s = { "<cmd>lua require('user.telescope.custom-finders').git_status()<cr>", "Git Status" },
-    },
-    l = {
-      name = "+Lsp",
-      a = { "<cmd>lua require('user.telescope.custom-finders').code_actions()<CR>", "Code Actions" }
-    },
-  }
-
-  lvim.builtin.which_key.mappings["t"] = {
-    name = "+Trouble",
-    r = { "<cmd>Trouble lsp_references<cr>", "References" },
-    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-    i = { "<cmd>Trouble lsp_implementations<cr>", "Implementations" },
-    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-    w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
-  }
-
-  lvim.builtin.which_key.mappings["R"] = {
-    name = "Replace",
-    p = { "<cmd>lua require('spectre').open()<cr>", "Project" },
-    f = { "<cmd>lua require('spectre').open_file_search({select_word=true})<cr>", "Current Buffer" },
-    w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
-  }
-
-  lvim.builtin.which_key.mappings["r"] = {
-    name = "File Replace",
-    f = { ":%s,<C-r><C-w>,", "Replace word under cursor on file" },
-    l = { ":s,<C-r><C-w>,", "Replace word under cursor in line" },
-  }
-
-
-  lvim.builtin.which_key.mappings["d"] = {
-    name = "+dotfiles",
-    d = { "<cmd>lua require('user.telescope.custom-finders').find_dotfiles()<CR>", "Find dotfiles" },
-    g = { "<cmd>lua require('user.telescope.custom-finders').grep_dotfiles()<CR>", "Grep dotfiles" },
-  }
-
-  lvim.builtin.which_key.mappings["P"] = {
-    name = "+Phpactor",
-    c = {
-      name = "+Class",
-      e = { ":PhpactorClassExpand<CR>", "Phpactor class expand" },
-      i = { ":PhpactorClassInflect<CR>", "Phpactor class inflect" },
-      n = { ":PhpactorClassNew<CR>", "Phpactor class new" }
-    }
-  }
-
-  lvim.builtin.which_key.mappings["o"] = {
-    name = "+Neorg",
-    n = { "<Plug>(neorg.dirman.new-note)", "New note" },
-    h = { "<Plug>(neorg.esupports.hop.hop-link)", "Jump to Link"  },
-    H = { "<Plug>(neorg.esupports.hop.hop-link.vsplit)", "Open link vsplit" },
-    t = { ":Neorg toggle-concealer<CR>", "Toggle concealer" },
-    r = { ":Neorg return<CR>", "Return" },
-    i = { ":Neorg index<CR>", "Index" },
-    w = { ":Neorg workspace ", "Workspaces" },
-    m = { ":Neorg inject-metadata<CR>", "Inject metadata" },
-    M = { ":Neorg update-metadata<CR>", "Update metadata" },
-    S = { ":Neorg generate-workspace-summary<CR>", "Insert summary" },
-    j = {
-      name = "+Journal",
-      t = { ":Neorg journal today<CR>", "Today" },
-      y = { ":Neorg journal yesterday<CR>", "Yesterday" },
-      c = { ":Neorg journal custom<CR>", "Custom" },
-    },
-    d = {
-      name = "+Docs",
-      d = { ":help neorg<CR>", "Docs" },
-      c = { ":help neorg-cheatsheet<CR>", "Cheatsheet" },
-    },
+    { prefix .. "od", group = "Docs" },
+    -- Docs Group
+    { prefix .. "odd", cmd = ":help neorg<CR>", desc = "Docs", group = "Docs" },
+    { prefix .. "odc", cmd = ":help neorg-cheatsheet<CR>", desc = "Cheatsheet", group = "Docs" },
   }
 end
 
--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,145 +1,171 @@
 local M = {}
 
-M.init = function ()
-  local frontend_file_types = { "html", "css", "javascript", "vue", "scss" }
-  local fugitive_commands = { "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse", "GRemove", "GRename", "Glgrep", "Gedit" }
+M.init = function()
+	local frontend_file_types = { "html", "css", "javascript", "vue", "scss" }
+	local fugitive_commands = {
+		"G",
+		"Git",
+		"Gdiffsplit",
+		"Gread",
+		"Gwrite",
+		"Ggrep",
+		"GMove",
+		"GDelete",
+		"GBrowse",
+		"GRemove",
+		"GRename",
+		"Glgrep",
+		"Gedit",
+	}
 
-  lvim.colorscheme = "catppuccin"
-  lvim.plugins = {
-    --- DOCS: dependencies
-    { "rcarriga/nvim-notify", -- not needed
-      config = function ()
-        require("notify").setup({
-          background_colour = "#000000",
-          render = "compact",
-          timeout = 1000, -- Duration in milliseconds
-          fps = 60, -- Animation frame rate
-          priority = 1000, -- Priority of the notification
-          icons = {
-            ERROR = "",
-            WARN = "",
-            INFO = "",
-            DEBUG = "",
-            TRACE = "✎",
-          },
-        })
-      end
-    },
-    --- DOCS: UI
-    { "catppuccin/nvim", -- Theme -- installed
-      name = "catppuccin",
-      init = function()
-        vim.g.catppuccin_flavour = require("user.config.theme").catppuccin_theme
-      end,
-      config = function()
-        require("user.config.theme").catppuccin()
-        vim.cmd [[colorscheme catppuccin]]
-      end
-    },
-    --- DOCS: Workflow
-    { "tpope/vim-fugitive", cmd = fugitive_commands },  --  Git management, commit, push and more
-    { "tpope/vim-rhubarb" },
-    { "ray-x/lsp_signature.nvim", -- Function signatures -- installed
-      event = "InsertEnter",
-      config = function()
-        require("user.plugins._signature").config()
-      end
-    },
-    { "folke/trouble.nvim", -- Better diagnostics, references and more -- installed
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      cmd = "Trouble",
-      event = "VeryLazy",
-      config = function()
-        require("trouble").setup({
-          focus = true,
-          preview = {
-            type = "float",
-            relative = "win",
-            border = "rounded",
-            title = "Preview",
-            title_pos = "center",
-            position = { 0, -2 },
-            size = { width = 0.4, height = 0.3 },
-            zindex = 200,
-          },
-        })
-      end
-    },
-    { "folke/noice.nvim", -- installed
-      event = "VeryLazy",
-      dependencies = {
-        "MunifTanjim/nui.nvim", -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "rcarriga/nvim-notify", -- `nvim-notify` is only needed, if you want to use the notification view.
-      },
-      config = function()
-        require("user.plugins._noice").config()
-      end
-    },
-    -- Better quickfix preview and list
-    { "kevinhwang91/nvim-bqf",
-      config = function()
-        require("user.plugins._bqf").config()
-      end,
-      event = "VeryLazy",
-    },
-    -- Find and replace across the whole project
-    { "windwp/nvim-spectre",
-      event = "VeryLazy",
-      config = function()
-        require("user.plugins._spectre").config()
-      end,
-    },
-    -- Motion provider
-    { "folke/flash.nvim",
-      event = "VeryLazy",
-      config = function()
-        require("user.plugins._flash").config()
-      end,
-      keys = require("user.plugins._flash").keys
-    },
-    --- DOCS: Language
-    { "mattn/emmet-vim", ft = frontend_file_types },
-    { "aca/emmet-ls", ft = frontend_file_types },
-    --- DOCS: Utility
-    { "norcalli/nvim-colorizer.lua", event = "VeryLazy" }, --  Display hex colors preview
-    { "tpope/vim-repeat", event = "VeryLazy" }, -- Repeat last command by pressing . (dot)
-    { "tpope/vim-surround", keys = { "c","d","y" }, event = "VeryLazy" }, -- Change surround brackets, quotes and more
-    { "kchmck/vim-coffee-script", event = "BufRead" }, -- Syntax highlight for Coffescript
-    { "folke/todo-comments.nvim", -- Highlight TODO, FIXME, etc
-      dependencies = { "nvim-lua/plenary.nvim" },
-      event = "VeryLazy",
-      config = function()
-        require("user.plugins._todo_comments").config()
-      end,
-    },
-    { "zbirenbaum/copilot.lua", -- AI code completion
-      cmd = "Copilot",
-      event = "BufRead",
-      config = function()
-        require("copilot").setup({
-          suggestion = { enabled = false },
-          panel = { enabled = false }
-        })
-      end
-    },
-    { "zbirenbaum/copilot-cmp", -- Copilot completion
-      dependencies = { "zbirenbaum/copilot.lua" },
-      event = "BufRead",
-      config = function ()
-        require("copilot_cmp").setup()
-      end
-    },
-    { "nvim-neorg/neorg", -- Neorg
-      -- build = ":Neorg sync-parsers",
-      ft = { "norg" },
-      dependencies = { "nvim-lua/plenary.nvim" },
-      lazy = false,
-      version = "*",
-      config = function()
-        require("user.plugins._neorg").config()
-      end
-    }
-  }
+	lvim.colorscheme = "catppuccin"
+	lvim.plugins = {
+		--- DOCS: dependencies
+		{
+			"rcarriga/nvim-notify", -- not needed
+			config = function()
+				require("notify").setup({
+					background_colour = "#000000",
+					render = "compact",
+					timeout = 1000, -- Duration in milliseconds
+					fps = 60, -- Animation frame rate
+					priority = 1000, -- Priority of the notification
+					icons = {
+						ERROR = "",
+						WARN = "",
+						INFO = "",
+						DEBUG = "",
+						TRACE = "✎",
+					},
+				})
+			end,
+		},
+		--- DOCS: UI
+		{
+			"catppuccin/nvim", -- Theme -- installed
+			name = "catppuccin",
+			init = function()
+				vim.g.catppuccin_flavour = require("user.config.theme").catppuccin_theme
+			end,
+			config = function()
+				require("user.config.theme").catppuccin()
+				vim.cmd([[colorscheme catppuccin]])
+			end,
+		},
+		--- DOCS: Workflow
+		{ "tpope/vim-fugitive", cmd = fugitive_commands }, --  Git management, commit, push and more
+		{ "tpope/vim-rhubarb" },
+		{
+			"ray-x/lsp_signature.nvim", -- Function signatures -- installed
+			event = "InsertEnter",
+			config = function()
+				require("user.plugins._signature").config()
+			end,
+		},
+		{
+			"folke/trouble.nvim", -- Better diagnostics, references and more -- installed
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			cmd = "Trouble",
+			event = "VeryLazy",
+			config = function()
+				require("trouble").setup({
+					focus = true,
+					preview = {
+						type = "float",
+						relative = "win",
+						border = "rounded",
+						title = "Preview",
+						title_pos = "center",
+						position = { 0, -2 },
+						size = { width = 0.4, height = 0.3 },
+						zindex = 200,
+					},
+				})
+			end,
+		},
+		{
+			"folke/noice.nvim", -- installed
+			event = "VeryLazy",
+			dependencies = {
+				"MunifTanjim/nui.nvim", -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+				"rcarriga/nvim-notify", -- `nvim-notify` is only needed, if you want to use the notification view.
+			},
+			config = function()
+				require("user.plugins._noice").config()
+			end,
+		},
+		-- Better quickfix preview and list
+		{
+			"kevinhwang91/nvim-bqf",
+			config = function()
+				require("user.plugins._bqf").config()
+			end,
+			event = "VeryLazy",
+		},
+		-- Find and replace across the whole project
+		{
+			"windwp/nvim-spectre",
+			event = "VeryLazy",
+			config = function()
+				require("user.plugins._spectre").config()
+			end,
+		},
+		-- Motion provider
+		{
+			"folke/flash.nvim",
+			event = "VeryLazy",
+			config = function()
+				require("user.plugins._flash").config()
+			end,
+			keys = require("user.plugins._flash").keys,
+		},
+		--- DOCS: Language
+		{ "mattn/emmet-vim", ft = frontend_file_types },
+		{ "aca/emmet-ls", ft = frontend_file_types },
+		--- DOCS: Utility
+		{ "norcalli/nvim-colorizer.lua", event = "VeryLazy" }, --  Display hex colors preview
+		{ "tpope/vim-repeat", event = "VeryLazy" }, -- Repeat last command by pressing . (dot)
+		{ "tpope/vim-surround", keys = { "c", "d", "y" }, event = "VeryLazy" }, -- Change surround brackets, quotes and more
+		{ "kchmck/vim-coffee-script", event = "BufRead" }, -- Syntax highlight for Coffescript
+		{
+			"folke/todo-comments.nvim", -- Highlight TODO, FIXME, etc
+			dependencies = { "nvim-lua/plenary.nvim" },
+			event = "VeryLazy",
+			config = function()
+				require("user.plugins._todo_comments").config()
+			end,
+		},
+		{
+			"zbirenbaum/copilot.lua", -- AI code completion
+			cmd = "Copilot",
+			event = "BufRead",
+			config = function()
+				require("copilot").setup({
+					suggestion = { enabled = false },
+					panel = { enabled = false },
+				})
+			end,
+		},
+		{
+			"zbirenbaum/copilot-cmp", -- Copilot completion
+			dependencies = { "zbirenbaum/copilot.lua" },
+			event = "BufRead",
+			config = function()
+				require("copilot_cmp").setup()
+			end,
+		},
+		{
+			"nvim-neorg/neorg", -- Neorg
+			-- build = ":Neorg sync-parsers",
+			ft = { "norg" },
+			dependencies = { "nvim-lua/plenary.nvim" },
+			lazy = false,
+			version = "*",
+			config = function()
+				require("user.plugins._neorg").config()
+			end,
+		},
+	}
 end
 
 return M
